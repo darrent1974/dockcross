ee744ead88f178ca1a50b39da4fcfa5cfe874c67
Darren Thompson <darren.thompson@csiro.au>
2018-11-01 14:49:13 +1100

    cleanup all cuda files in the /tmp folder after installation of cuda

--- imagefiles/install-cuda.sh
+++ imagefiles/install-cuda.sh
@@ -21,7 +21,7 @@ elif [[ "${CUDA_VERSION}" == "8.0" ]]; then
 	export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}	
 	export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64 ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 	
-	rm cuda_8.0.61_375.26_linux-run
+	rm cuda*
 else
 	echo >&2 'error: Unsupported CUDA Version' ${CUDA_VERSION}
   	exit 1	
